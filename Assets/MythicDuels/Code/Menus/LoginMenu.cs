c2b0381edc94c9e4635070fe3c32bf1bad9d05d5
Sanrro10 <i.glzsr@opendeusto.es>
2021-04-19 09:45:15 +0200

    Photon

---
+++ Assets/MythicDuels/Code/Menus/LoginMenu.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CardGame.Menus.Main
+{
+    public class LoginMenu : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField nameInputField = null;
+        [SerializeField] private Button continueButton = null;
+
+        private const string PlayerPrefNameKey = "PlayerName";
+
+        private void Start() => SetUpInputField();
+
+        private void SetUpInputField()
+        {
+            if (!PlayerPrefs.HasKey(PlayerPrefNameKey)) { return; }
+
+            string defaultName = PlayerPrefs.GetString(PlayerPrefNameKey);
+
+            nameInputField.text = defaultName;
+
+            SetPlayerName(defaultName);
+        }
+
+        public void SetPlayerName(string name)
+        {
+            continueButton.interactable = !string.IsNullOrEmpty(name);
+        }
+
+        public void SavePlayerName()
+        {
+            string playerName = nameInputField.text;
+
+            PhotonNetwork.NickName = playerName;
+
+            PlayerPrefs.SetString(PlayerPrefNameKey, playerName);
+        }
+    }
+}
